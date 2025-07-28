88d699de950c8d8654a3e007e33e2f6b8a010344
copycd <copycd@daum.net>
2025-07-28 11:28:03 +0900

    - 불필요한 옶녀들 감춤.

--- Obj2Tiles.Common/Options.cs
+++ Obj2Tiles.Common/Options.cs
@@ -11,13 +11,13 @@ public sealed class Options
     [Value(1, MetaName = "Output", Required = true, HelpText = "Output folder.")]
     public string Output { get; set; }
 
-    [Option('s', "stage", Required = false, HelpText = "Stage to stop at (Decimation, Splitting, Tiling)", Default = Stage.Tiling)]
+    [Option('s', "stage", Required = false, HelpText = "Stage to stop at (Decimation, Splitting)", Default = Stage.Splitting)]
     public Stage StopAt { get; set; }
 
-    [Option('d', "limitLength", Required = false, HelpText = "How many tiles divisions", Default = 10)]
+    [Option('d', "limitLength", Required = false, HelpText = "Limited size", Default = 10)]
     public double LimitLength { get; set; }
     
-    [Option('z', "zsplit", Required = false, HelpText = "Splits along z-axis too", Default = false)]
+    [Option('z', "zsplit", Required = false, HelpText = "Splits along z-axis too", Default = true)]
     public bool ZSplit { get; set; }    
     
     [Option('l', "lods", Required = false, HelpText = "How many levels of details", Default = 3)]
@@ -26,13 +26,10 @@ public sealed class Options
     [Option('k', "keeptextures", Required = false, HelpText = "Keeps original textures", Default = false)]
     public bool KeepOriginalTextures { get; set; }
     
-    [Option("lat", Required = false, HelpText = "Latitude of the mesh", Default = null)]
     public double? Latitude { get; set; }
     
-    [Option("lon", Required = false, HelpText = "Longitude of the mesh", Default = null)]
     public double? Longitude { get; set; }
     
-    [Option("alt", Required = false, HelpText = "Altitude of the mesh (meters)", Default = 0)]
     public double Altitude { get; set; }
 
     [Option("scale", Required = false, HelpText = "Scale for data if using units other than meters ( 1200.0/3937.0 for survey ft)", Default = 1.0)]
